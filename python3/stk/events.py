5fcb7b42b0c0693adc493a1c1defe327c7391d8d
47452636 <p.kools@student.ru.nl>
2023-11-28 14:12:52 +0100

    add dist folder with naoqiconnection.exe

--- python3/stk/events.py
+++ python3/stk/events.py
@@ -1,43 +1,44 @@
 import time
 
+import time
+
 class EventHelper(object):
 
     def __init__(self, bridge):
-    	self.bridge = bridge
-    	self.bridge.set_eventhandler(self)
-    	self.callbacks = dict()
-    	self.is_waiting = False
-
+        self.bridge = bridge
+        self.bridge.set_eventhandler(self)
+        self.callbacks = dict()
+        self.is_waiting = False
 
     def connect(self, event, callback):
-    	if not event in self.callbacks:
-    		self.callbacks[event] = list()
+        if not event in self.callbacks:
+            self.callbacks[event] = list()
 
-    	self.callbacks[event].append(callback)
+        self.callbacks[event].append(callback)
 
-    	self.bridge.sendMessage("python27:self.events.connect(\"" + event + "\", self." + event.replace('/','_') + ")")
+        self.bridge.sendMessage("python27:self.events.connect(\"" + event + "\", self." + event.replace('/','_') + ")")
 
     def disconnect(self, event):
-    	if event in self.callbacks:
-    		self.callbacks[event] = list()
+        if event in self.callbacks:
+            self.callbacks[event] = list()
 
-    	self.bridge.sendMessage("python27:self.events.disconnect(\"" + event + "\")")
+        self.bridge.sendMessage("python27:self.events.disconnect(\"" + event + "\")")
 
     def wait_for(self, event):
-    	self.is_waiting = True
+        self.is_waiting = True
 
-    	self.bridge.sendMessage("python27:self.events.connect(\"" + event + "\", self." + event.replace('/','_') + ")")
+        self.bridge.sendMessage("python27:self.events.connect(\"" + event + "\", self." + event.replace('/','_') + ")")
 
-    	while self.is_waiting:
-    		time.sleep(0.05)
+        while self.is_waiting:
+            time.sleep(0.05)
 
-    	self.bridge.sendMessage("python27:self.events.disconnect(\"" + event + "\")")
+        self.bridge.sendMessage("python27:self.events.disconnect(\"" + event + "\")")
 
 
     def trigger_callback(self, event, params):
-    	if self.is_waiting:
-    		self.is_waiting = False
+        if self.is_waiting:
+            self.is_waiting = False
 
-    	else:
-	    	for c in self.callbacks[event.replace('_', '/')]:
-	    		c(params)
+        else:
+            for c in self.callbacks[event.replace('_', '/')]:
+                c(params)
